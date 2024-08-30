d8b355a83acb44fcbdf6d9667cc03615a18990e2
Alex Chon <achon@uwaterloo.ca>
2024-08-30 08:16:17 -0400

    Add GetStarted Component to the Home Page Commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,11 +3,13 @@ import Nav from '../components/Nav';
 import axios from '../helpers/axios';
 import DisplayPost from '../components/DisplayPost';
 import styles from '../styles/HomePage.module.css';
+import GetStarted from '../components/GetStarted';
 
 const HomePage = () => {
   // Write in TypeScript
   const [initialPosts, setInitialPosts] = useState(null);
 
+  // On mount, fetch the top 5 liked posts, then set it to initialPosts
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -26,20 +28,25 @@ const HomePage = () => {
     <>
       <Nav />
       <h1 className={styles.yourFeed}>Your Feed</h1>
-      {initialPosts &&
-        initialPosts.map((post) => {
-          return (
-            <DisplayPost
-              key={post._id}
-              description={post.description}
-              title={post.title}
-              comments={post.comments}
-              likes={post.likes}
-              threadName={post.threadName}
-              username={post.user_id}
-            />
-          );
-        })}
+      <div className={styles.homePageContainer}>
+        <div className='posts'>
+          {initialPosts &&
+            initialPosts.map((post) => {
+              return (
+                <DisplayPost
+                  key={post._id}
+                  description={post.description}
+                  title={post.title}
+                  comments={post.comments}
+                  likes={post.likes}
+                  threadName={post.threadName}
+                  username={post.user_id}
+                />
+              );
+            })}
+        </div>
+        <GetStarted />
+      </div>
     </>
   );
 };
